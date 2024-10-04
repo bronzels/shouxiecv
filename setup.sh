d709637b4b13f333a3e28c694b94d864ebf3b691
apple <bronzels@hotmail.com>
2024-10-04 14:16:17 +0800

    1,first time setup. 2, opencv

---
+++ setup.sh
@@ -0,0 +1,7 @@
+pip install jupyter notebook
+pip install opencv-python opencv-contrib-python opencv-python-headless
+#-4.10.0.84
+pip install matplotlib 
+git clone git@github.com:AccumulateMore/OpenCV OpenCV2
+ln -s OpenCV/01_Picture 01_Picture
+ln -s OpenCV/02_Video 02_Video
