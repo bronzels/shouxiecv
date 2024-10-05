1d2be1f828d85ee5205a0c3f84e6843c3c93ae5a
apple <bronzels@hotmail.com>
2024-10-05 13:08:51 +0800

    1,opencv credit card no extraction.

--- setup.sh
+++ setup.sh
@@ -2,8 +2,21 @@ pip install jupyter notebook
 pip install opencv-python opencv-contrib-python opencv-python-headless
 #-4.10.0.84
 pip install matplotlib 
+
 git clone git@github.com:AccumulateMore/OpenCV
-mkdir biliopencv
-cd biliopencv
+mkdir bili-opencv
+cd bili-opencv
 ln -s ../OpenCV/01_Picture 01_Picture
 ln -s ../OpenCV/02_Video 02_Video
+
+#https://www.haolizi.net/example/view_271111.html
+#下载代码解压
+mkdir bili-template-matching-ocr
+cd bili-template-matching-ocr
+ln -s ../template-matching-ocr/images images
+ln -s ../template-matching-ocr/ocr_a_reference.png ocr_a_reference.png
+
+
+git clone git@github.com:zhongqiangwu960812/OpenCVLearning
+git clone git@github.com:ZhiqiangHo/Opencv-Computer-Vision-Practice-Python-
+git clone git@github.com:YvanYan/image_processing
